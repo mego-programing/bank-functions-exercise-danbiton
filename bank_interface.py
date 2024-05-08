43ca90bdda8882341ae2605e5c896a361fe14150
Daniel Biton <biton123654@gmail.com>
2024-05-08 16:05:37 +0300

    first function

--- bank_interface.py
+++ bank_interface.py
@@ -1,1 +1,10 @@
-# write your code here:
+variable_balance = 0 
+
+def deposit(amount):
+    global variable_balance
+    variable_balance += amount
+    return variable_balance
+
+amount1 = int(input("how macch money you deposit :")) 
+
+print(deposit(amount1))
